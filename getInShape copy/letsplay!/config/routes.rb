c7c94351ad98ac910c63cca199654bff06934b8a
Aaron Z <aaronz@mit.edu>
2014-01-21 23:32:31 -0500

    trying to show

--- getInShape copy/letsplay!/config/routes.rb
+++ getInShape copy/letsplay!/config/routes.rb
@@ -12,7 +12,7 @@ Letsplay::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
 
-   root 'welcome#index'
+   root 'user_sports#index'
 
 
   # Example of regular route:
