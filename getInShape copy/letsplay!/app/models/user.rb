63ca102bb2b4820c85c8f2148b57d90992a592c4
emmachristie <emchris7@mit.edu>
2014-01-22 20:58:58 -0500

    gems

--- getInShape copy/letsplay!/app/models/user.rb
+++ getInShape copy/letsplay!/app/models/user.rb
@@ -1,13 +1,11 @@
 class User < ActiveRecord::Base
 	# validates_presence_of :Sports
 	# has_many :Sports
-<<<<<<< HEAD
+
 	#attr_accessible :Location, :Latitude, :Longitude
 	geocoded_by :Location
 	after_validation :geocode, :if => :Location_changed?
 
-=======
-	acts_as_messageable
->>>>>>> d74a3e02a97e8e733d1f751c09d87610bf2909e3
+
 	
 end
