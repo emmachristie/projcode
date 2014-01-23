5ee7266dc3495b538627806c99aed387cb48b3ba
Aaron Z <aaronz@mit.edu>
2014-01-22 20:58:12 -0500

    delete

--- getInShape copy/letsplay!/app/models/user.rb
+++ getInShape copy/letsplay!/app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
 	# validates_presence_of :Sports
 	# has_many :Sports
-<<<<<<< HEAD
+
 	#attr_accessible :Location, :Latitude, :Longitude
 	geocoded_by :Location
 	after_validation :geocode, :if => :Location_changed?
-
-=======
-	acts_as_messageable
->>>>>>> d74a3e02a97e8e733d1f751c09d87610bf2909e3
 	
+	acts_as_messageable
+
+	def name
+		return @user.FirstName @user.LastName
+	end
+
 end
